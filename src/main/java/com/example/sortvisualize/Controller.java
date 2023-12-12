0b5c7b63c4b02757b8c8d00346ed708b2fde9ac2
Teflon Dervish <voltvilty45@gmail.com>
2023-12-12 21:22:13 +0500

    Make sorts

--- src/main/java/com/example/sortvisualize/Controller.java
+++ src/main/java/com/example/sortvisualize/Controller.java
@@ -1,6 +1,7 @@
 package com.example.sortvisualize;
 
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
@@ -15,6 +16,9 @@ import java.util.function.UnaryOperator;
 import static javafx.collections.FXCollections.observableArrayList;
 
 public class Controller {
+
+    @FXML
+    private Button resumeButton, stopButton;
 
     @FXML
     private Canvas canvasBubbleSort;
@@ -34,8 +38,15 @@ public class Controller {
     @FXML
     private TextField timeDelay;
 
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private TextField iterationCount;
+    private volatile boolean paused = false;
     private SortingAlgorithm sortingAlgorithm;
     GraphicsContext gc;
+    Thread mainTread;
 
     @FXML
     void initialize(){
@@ -55,17 +66,24 @@ public class Controller {
         count.setTextFormatter(textFormatter);
 
         // Выбор типа сортировки
-        ObservableList<String> items = observableArrayList("Сортировка пузырьком", "Быстрая сортировка");
+        ObservableList<String> items = observableArrayList("InsertionSort", "SelectionSort", "BubbleSort",
+                                                            "QuickSort", "ShellSort", "ShakerSort",
+                                                            "GnomeSort", "HeapSort",
+                                                            "MergeSort", "CombSort", "CountingSort",
+                                                            "TournamentSort");
         chosenSort.setItems(items);
-        chosenSort.setValue("Сортировка пузырьком");
+        chosenSort.setValue("BubbleSort");
 
         // Установка времени задержки
-        textFormatter = new TextFormatter<>(new IntegerStringConverter(), 100, filter);
+        textFormatter = new TextFormatter<>(new IntegerStringConverter(), 10, filter);
         timeDelay.setTextFormatter(textFormatter);
 
+        // Для паузы
+        stopButton.setOnAction(event -> {mainTread.interrupt(); sortingAlgorithm.drawArray();});
+
         // Массив для сортировки
-        sortingAlgorithm = new SortingAlgorithm(10);
-        drawArray();
+        sortingAlgorithm = new SortingAlgorithm(gc, 10);
+        sortingAlgorithm.drawArray();
     }
 
     @FXML // Slider
@@ -84,20 +102,71 @@ public class Controller {
     }
 
     @FXML
-    void setTimeDelay(ActionEvent event) {
-
-    }
-
-    @FXML
     void shuffle(ActionEvent event) {
         sortingAlgorithm.setLengthArray((int) countSlider.getValue());
-        drawArray();
+        sortingAlgorithm.drawArray();
     }
 
     @FXML
-    void startOnCurrentWindow(ActionEvent event) {
-        sortingAlgorithm.bubbleSort();
-        drawArray();
+    void startOnCurrentWindow(ActionEvent event) throws InterruptedException {
+        sortingAlgorithm.setLengthArray((int) countSlider.getValue());
+        sortingAlgorithm.drawArray();
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                int delay = Integer.parseInt(timeDelay.getText());
+                sortingAlgorithm.iterration = 0;
+                switch (chosenSort.getValue()){
+                    case "InsertionSort":   sortingAlgorithm.InsertionSort(delay); break;
+                    case "SelectionSort":   sortingAlgorithm.SelectionSort(delay); break;
+                    case "BubbleSort":  sortingAlgorithm.BubbleSort(delay); break;
+                    case "QuickSort":   sortingAlgorithm.QuickSort(0, sortingAlgorithm.getArray().size() - 1, delay); break;
+                    case "ShellSort":   sortingAlgorithm.ShellSort(delay); break;
+                    case "ShakerSort":  sortingAlgorithm.ShakerSort(delay); break;
+                    case "GnomeSort":   sortingAlgorithm.GnomeSort(delay); break;
+                    case "HeapSort":    sortingAlgorithm.HeapSort(delay); break;
+                    case "MergeSort":   sortingAlgorithm.MergeSort(delay); break;
+                    case "CombSort":    sortingAlgorithm.CombSort(delay); break;
+                    case "CountingSort":    sortingAlgorithm.CountingSort(delay); break;
+                    case "TournamentSort":  sortingAlgorithm.TournamentSort(delay); break;
+                }
+
+                iterationCount.setText(String.valueOf(sortingAlgorithm.iterration));
+                return null;
+            }
+        };
+        mainTread = new Thread(task);
+        mainTread.start();
+
+    }
+
+    @FXML
+    void continueSort(ActionEvent event) throws InterruptedException{
+        System.out.println(1);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                int delay = Integer.parseInt(timeDelay.getText());
+                System.out.println(chosenSort.getValue());
+                switch (chosenSort.getValue()){
+                    case "InsertionSort":   sortingAlgorithm.InsertionSort(delay);
+                    case "SelectionSort":   sortingAlgorithm.SelectionSort(delay);
+                    case "BubbleSort":  sortingAlgorithm.BubbleSort(delay);
+                    case "QuickSort":   sortingAlgorithm.QuickSort(0, sortingAlgorithm.getArray().size(), delay);
+                    case "ShellSort":   sortingAlgorithm.ShellSort(delay);
+                    case "ShakerSort":  sortingAlgorithm.ShakerSort(delay);
+                    case "GnomeSort":   sortingAlgorithm.GnomeSort(delay);
+                    case "HeapSort":    sortingAlgorithm.HeapSort(delay);
+                    case "MergeSort":   sortingAlgorithm.MergeSort(delay);
+                    case "CombSort":    sortingAlgorithm.CombSort(delay);
+                    case "CountingSort":    sortingAlgorithm.CountingSort(delay);
+                    case "TournamentSort":  sortingAlgorithm.TournamentSort(delay);
+                }
+                return null;
+            }
+        };
+        mainTread = new Thread(task);
+        mainTread.start();
     }
 
     @FXML
@@ -105,18 +174,4 @@ public class Controller {
 
     }
 
-    @FXML
-    void stopSort(ActionEvent event) {
-
-    }
-
-    private void drawArray(){
-        gc.clearRect(0, 0, canvasBubbleSort.getWidth(), canvasBubbleSort.getHeight());
-        int arr[] = sortingAlgorithm.getArray();
-        int arr_width = (int) (canvasBubbleSort.getWidth() / arr.length);
-        int arr_height = (int) (canvasBubbleSort.getHeight() / arr.length);
-        for (int i = 0; i < arr.length; i ++){
-            gc.fillRect(arr_width * i, arr_height * (arr.length-arr[i]), arr_width, arr_height * arr[i]);
-        }
-    }
 }
--- src/main/java/com/example/sortvisualize/SortingAlgorithm.java
+++ src/main/java/com/example/sortvisualize/SortingAlgorithm.java
@@ -1,55 +1,411 @@
 package com.example.sortvisualize;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
-import java.util.Timer;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class SortingAlgorithm {
 
-    private int[] array;
-    SortingAlgorithm(int length){
+    public int iterration;
+    GraphicsContext gc;
+    private ArrayList<Integer> array;
+    SortingAlgorithm(GraphicsContext gc, int length){
         this.array = generateArray(length);
+        this.gc = gc;
     }
-    private int[] generateArray(int length) {
-        int[] arr = new int[length];
-        for (int i = 0; i < length; i++) {
-            arr[i] = i + 1;
-        }
-        Random rand = new Random();
-        for (int i = arr.length - 1; i > 0; i--) {
-            int index = rand.nextInt(i + 1);
-            swap(arr, i, index);
-        }
-        return arr;
+    private ArrayList<Integer> generateArray(int length) {
+        ArrayList<Integer> initialList = new ArrayList<>();
+        for (int i = 1; i <= length; i++)
+            initialList.add(i);
+        Collections.shuffle(initialList);
+        return initialList;
     }
-    public int[] getArray(){
+    public ArrayList<Integer> getArray(){
         return array;
     }
     public void setLengthArray(int length) {
         this.array = generateArray(length);
     }
-    private void swap(int[] arr, int index1, int index2){
-        if (index1 >= 0 && index1 < arr.length && index2 >= 0 && index2 < arr.length) {
-            int temp = arr[index1];
-            arr[index1] = arr[index2];
-            arr[index2] = temp;
-        } else {
-            System.out.println("Invalid indices for swap");
+
+
+    // Рисование на холсте
+    public void drawArray(){
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+        gc.setFill(Color.BLACK);
+        int arr_width = (int) (gc.getCanvas().getWidth() / array.size());
+        int arr_height = (int) (gc.getCanvas().getHeight() / array.size());
+        for (int i = 0; i < array.size(); i ++){
+            gc.fillRect(arr_width * i, arr_height * (array.size()-array.get(i)), arr_width, arr_height * array.get(i));
         }
     }
-    public void bubbleSort() {
-        int n = array.length;
+    public void drawCurrentPos(int i, Color color){
+        int arr_width = (int) (gc.getCanvas().getWidth() / array.size());
+        int arr_height = (int) (gc.getCanvas().getHeight() / array.size());
+        gc.setFill(color);
+        gc.fillRect(arr_width * i, arr_height * (array.size()-array.get(i)), arr_width, arr_height * array.get(i));
+    }
+
+
+    // Сортировки
+    public void InsertionSort(int delay) throws InterruptedException {
+        for (int i = 1; i < array.size(); i++) {
+            int key = array.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && array.get(j) > key) {
+                array.set(j + 1, array.get(j));
+                j = j - 1;
+                iterration++;
+                drawArray();
+                drawCurrentPos(j, Color.RED);
+                Thread.sleep(delay);
+            }
+
+            array.set(j + 1, key);
+        }
+    }
+    public void SelectionSort(int delay) throws InterruptedException {
+        int n = array.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (array.get(j) < array.get(minIndex)) {
+                    minIndex = j;
+                }
+                iterration++;
+                drawArray();
+                drawCurrentPos(j, Color.RED);
+                Thread.sleep(delay);
+            }
+            swap(array, minIndex, i);
+        }
+    }
+    public void BubbleSort(int delay) throws InterruptedException {
+        int n = array.size();
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array.get(j) > array.get(j + 1)) {
+                    swap(array, j, j + 1);
+                    swapped = true;
+                }
+                iterration++;
+                drawArray();
+                drawCurrentPos(j, Color.RED);
+                Thread.sleep(delay);
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    public void QuickSort(int low, int high, int delay) throws InterruptedException {
+        if (array.isEmpty())
+            return;
+
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        int opora = array.get(middle);
+        int i = low, j = high;
+        while (i <= j) {
+            while (array.get(i) < opora) {
+                i++;
+            }
+
+            while (array.get(j) > opora) {
+                j--;
+            }
+
+            if (i <= j) {
+                swap(array, i, j);
+                i++;
+                j--;
+                iterration++;
+                drawArray();
+                drawCurrentPos(i, Color.RED);
+                drawCurrentPos(j, Color.GREEN);
+                Thread.sleep(delay);
+            }
+        }
+
+        if (low < j)
+            QuickSort(low, j, delay);
+
+        if (high > i)
+            QuickSort(i, high, delay);
+    }
+    public void ShellSort(int delay) throws InterruptedException {
+        int n = array.size();
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = array.get(i);
+                int j;
+                for (j = i; j >= gap && array.get(j - gap) > temp; j -= gap) {
+                    array.set(j, array.get(j - gap));
+                    iterration++;
+                    drawArray();
+                    drawCurrentPos(j, Color.RED);
+                    Thread.sleep(delay);
+                }
+                array.set(j, temp);
+            }
+        }
+    }
+    public void ShakerSort(int delay) throws InterruptedException {
         boolean swapped;
         do {
             swapped = false;
-            for (int i = 1; i < n; i++) {
-                if (array[i - 1] > array[i]) {
-                    swap(array, i - 1, i);
+
+            for (int i = 0; i <= array.size() - 2; i++) {
+                if (array.get(i) > array.get(i + 1)) {
+                    swap(array, i, i + 1);
                     swapped = true;
                 }
+                iterration++;
+                drawArray();
+                drawCurrentPos(i, Color.RED);
+                Thread.sleep(delay);
             }
+
+            if (!swapped) {
+                break;
+            }
+
+            swapped = false;
+
+            for (int i = array.size() - 2; i >= 0; i--) {
+                if (array.get(i) > array.get(i + 1)) {
+                    swap(array, i, i + 1);
+                    swapped = true;
+                }
+                iterration++;
+                drawArray();
+                drawCurrentPos(i, Color.RED);
+                Thread.sleep(delay);
+            }
+
         } while (swapped);
     }
+    public void GnomeSort(int delay) throws InterruptedException {
+        int n = array.size();
+        int index = 0;
 
+        while (index < n) {
+            if (index == 0) {
+                index++;
+            }
+
+            if (array.get(index) >= array.get(index - 1)) {
+                index++;
+            } else {
+                swap(array, index, index - 1);
+                index--;
+            }
+            iterration++;
+            drawArray();
+            drawCurrentPos(index, Color.RED);
+            Thread.sleep(delay);
+        }
+    }
+    public void HeapSort(int delay) throws InterruptedException {
+        int n = array.size();
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            Heapify(array, n, i,delay);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            swap(array, 0, i);
+            Heapify(array, i, 0, delay);
+        }
+    }
+    private void Heapify(ArrayList<Integer> arr, int n, int i, int delay) throws InterruptedException {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr.get(left) > arr.get(largest)) {
+            largest = left;
+        }
+
+        if (right < n && arr.get(right) > arr.get(largest)) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            Heapify(arr, n, largest, delay);
+        }
+        iterration++;
+        drawArray();
+        drawCurrentPos(i, Color.RED);
+        Thread.sleep(delay);
+    }
+
+    public void MergeSort(int delay) throws InterruptedException {
+        mergeSort(array, delay);
+    }
+    private void mergeSort(ArrayList<Integer> arr, int delay) throws InterruptedException {
+        if (arr.size() > 1) {
+            int mid = arr.size() / 2;
+            ArrayList<Integer> left = new ArrayList<>(arr.subList(0, mid));
+            ArrayList<Integer> right = new ArrayList<>(arr.subList(mid, arr.size()));
+
+            mergeSort(left, delay);
+            mergeSort(right, delay);
+
+            merge(arr, left, right, delay);
+        }
+    }
+    private void merge(ArrayList<Integer> arr, ArrayList<Integer> left, ArrayList<Integer> right, int delay) throws InterruptedException {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i) < right.get(j)) {
+                arr.set(k++, left.get(i++));
+            } else {
+                arr.set(k++, right.get(j++));
+            }
+            iterration++;
+            drawArray();
+            drawCurrentPos(i, Color.RED);
+            Thread.sleep(delay);
+        }
+
+        while (i < left.size()) {
+            arr.set(k++, left.get(i++));
+            iterration++;
+            drawArray();
+            drawCurrentPos(i, Color.RED);
+            Thread.sleep(delay);
+        }
+
+        while (j < right.size()) {
+            arr.set(k++, right.get(j++));
+            iterration++;
+            drawArray();
+            drawCurrentPos(i, Color.RED);
+            Thread.sleep(delay);
+        }
+    }
+    public void CombSort(int delay) throws InterruptedException {
+        int n = array.size();
+        int gap = n;
+        boolean swapped = true;
+
+        while (gap != 1 || swapped) {
+            gap = getNextGap(gap);
+            swapped = false;
+
+            for (int i = 0; i < n - gap; i++) {
+                if (array.get(i) > array.get(i + gap)) {
+                    swap(array, i, i + gap);
+                    swapped = true;
+                }
+                iterration++;
+                drawArray();
+                drawCurrentPos(i, Color.RED);
+                Thread.sleep(delay);
+            }
+        }
+    }
+    private static int getNextGap(int gap) {
+        gap = (gap * 10) / 13;
+        return Math.max(gap, 1);
+    }
+    public void CountingSort(int delay) throws InterruptedException {
+        int max = findMaxValue(array);
+        int[] count = new int[max + 1];
+
+        for (int num : array) {
+            count[num]++;
+            iterration++;
+        }
+
+        int index = 0;
+        for (int i = 0; i <= max; i++) {
+            while (count[i] > 0) {
+                array.set(index++, i);
+                count[i]--;
+            }
+            iterration++;
+            drawArray();
+            drawCurrentPos(i, Color.RED);
+            Thread.sleep(delay);
+        }
+    }
+    public void TournamentSort(int delay) throws InterruptedException {
+        ArrayList<Integer> tree = new ArrayList<>(2 * array.size() - 1);
+
+        for (int i = 0; i < 2 * array.size() - 1; i++) {
+            tree.add(Integer.MAX_VALUE);
+        }
+
+        buildTree(array, tree, 0, array.size() - 1, 0, delay);
+        buildSortedArray(array, tree, 0, array.size() - 1, 0, delay);
+    }
+    private void buildTree(ArrayList<Integer> arr, ArrayList<Integer> tree, int start, int end, int treeNode, int delay) throws InterruptedException {
+        if (start == end) {
+            tree.set(treeNode, start);
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        buildTree(arr, tree, start, mid, 2 * treeNode + 1, delay);
+        buildTree(arr, tree, mid + 1, end, 2 * treeNode + 2, delay);
+
+        if (arr.get(tree.get(2 * treeNode + 1)) < arr.get(tree.get(2 * treeNode + 2))) {
+            tree.set(treeNode, tree.get(2 * treeNode + 1));
+        } else {
+            tree.set(treeNode, tree.get(2 * treeNode + 2));
+        }
+        iterration++;
+        drawArray();
+        drawCurrentPos(mid, Color.RED);
+        Thread.sleep(delay);
+    }
+    private void buildSortedArray(ArrayList<Integer> arr, ArrayList<Integer> tree, int start, int end, int treeNode, int delay) throws InterruptedException {
+        if (start == end) {
+            arr.set(start, tree.get(treeNode));
+            return;
+        }
+
+        int minIndex = tree.get(treeNode);
+        int mid = (start + end) / 2;
+
+        if (arr.get(minIndex) != Integer.MAX_VALUE) {
+            swap(arr, start, minIndex);
+            buildTree(arr, tree, start, mid, 2 * treeNode + 1, delay);
+            buildTree(arr, tree, mid + 1, end, 2 * treeNode + 2, delay);
+        }
+    }
+
+    // Вспомогательные функции
+    private static int findMinValue(List<Integer> arr) {
+        int min = Integer.MAX_VALUE;
+        for (int num : arr) {
+            min = Math.min(min, num);
+        }
+        return min;
+    }
+    private static int findMaxValue(List<Integer> arr) {
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+    private static void swap(List<Integer> arr, int i, int j){
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
 }
